a75607b17c145e6635eb467216861bb1ca225dbb
HomePC <melonfondue.7@gmail.com>
2020-06-07 18:56:42 +0900

    props type

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const User = (props) => {
   return <div>Hi, Iam {props.name}, and {props.age} years old !</div>
 }
+// デフォルト値を設定
+// User.defaultProps = {
+//   age: 1
+// }
 
-User.defaultProps = {
-  age: 1
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired //isRequired: 必須の場合は付与する
 }
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "Hanako", age: 35 },
-    { name: "mako" }
+    { name: "Noname" }
   ]
 
   return (
